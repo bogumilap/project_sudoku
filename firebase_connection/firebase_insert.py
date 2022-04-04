098e4089562198535bbb2622363a86d4b2c564bf
bpap <bogumila.papiernik@gmail.com>
2022-04-05 00:56:00 +0200

    changed path to json

--- firebase_connection/firebase_insert.py
+++ firebase_connection/firebase_insert.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("sudoku-ecca5-firebase-adminsdk-39oik-b57bb08c6c.json")
+cred = credentials.Certificate("../firebase_connection/sudoku-ecca5-firebase-adminsdk-39oik-b57bb08c6c.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://sudoku-ecca5-default-rtdb.europe-west1.firebasedatabase.app/'
 })
