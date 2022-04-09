37d7a99299a293e406766c56c2a952e2ddf4639f
bpap <bogumila.papiernik@gmail.com>
2022-04-09 17:50:13 +0200

    removed unnecessary code

--- frontend/levels.py
+++ frontend/levels.py
@@ -4,14 +4,9 @@ from kivy.uix.screenmanager import Screen
 
 kivy.require('2.1.0')
 
+
 class levelsWindow(Screen):
     pass
 
 
 kv = Builder.load_file('./levels.kv')
-
-
-# def __init__(self, **kwargs):
-#     super().__init__(**kwargs)
-
-
