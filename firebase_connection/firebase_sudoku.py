ea19ea69604f46b945fa9521cce01353dd3bb3c6
bpap <bogumila.papiernik@gmail.com>
2022-05-05 15:21:02 +0200

    update ranking

--- firebase_connection/firebase_sudoku.py
+++ firebase_connection/firebase_sudoku.py
@@ -53,7 +53,34 @@ def finishGame(uid, sudoku_id):  # update user's statistics and total points
     time = firebase_ref.getRef().child('history').child(str(uid)).child(str(sudoku_id)).child("time").get()
     seconds = time[0] * 3600 + time[1] * 60 + time[2]
     game_points = firebase_ref.getRef().child('history').child(str(uid)).child(str(sudoku_id)).child('game_points').get()
-    user_data.child('total_points').set(total_points + game_points * 10 / seconds)  
+    user_data.child('total_points').set(total_points + game_points * 10 / seconds)
+    updateRanking(uid)
+
+
+def updateRanking(uid):
+    top5 = firebase_ref.getRef().child("top5").get()
+    user = firebase_ref.getRef().child("users").child(uid).get()
+    users_points = user.get("total_points")
+    insert_index = -1
+    for i in range(len(top5)):
+        current = top5[i]
+        if current.get("total_points") < users_points:
+            insert_index = i
+            break
+
+    if insert_index != -1:
+        for i in range(4, insert_index):
+            better_user = firebase_ref.getRef().child("top5").child(i-1).get()
+            firebase_ref.getRef().child("top5").child(i).set(better_user)
+
+        data = {
+            'nick': user.get("nick"),
+            'no_played': user.get("no_played"),
+            'no_won': user.get("no_won"),
+            'total_points' : users_points
+        }
+        firebase_ref.getRef().child("top5").child(str(insert_index)).set(data)
+
 
 
 def getAllSolvedSudoku():
