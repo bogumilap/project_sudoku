54cb6205e1aa8e2bc6bc5831cb4bfaba850ed7bb
bogumilap <92338713+bogumilap@users.noreply.github.com>
2022-05-06 00:18:06 +0200

    Delete firebase_update.py

--- firebase_connection/firebase_update.py
+++
@@ -1,5 +0,0 @@
-from firebase import firebase
-
-firebase = firebase.FirebaseApplication("https://sudoku-ecca5-default-rtdb.europe-west1.firebasedatabase.app/", None)
-
-firebase.put('/top5/0', 'no_played', 2)
