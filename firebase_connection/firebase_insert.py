faf32e460cea5b07ca648829c62112897e554630
bpap <bogumila.papiernik@gmail.com>
2022-05-06 01:19:36 +0200

    removed unnecessary code

--- firebase_connection/firebase_insert.py
+++ firebase_connection/firebase_insert.py
@@ -1,5 +1,6 @@
 from firebase_connection import firebase_ref
 
+
 def addUser(uid, nick, email):  # adding new user to 'users' collection in firebase
     data = {
         'email': email,
@@ -10,40 +11,4 @@ def addUser(uid, nick, email):  # adding new user to 'users' collection in fireb
     }
     firebase_ref.getRef().child('users').child(str(uid)).set(data)
 
-
-# initializing ranking
-top5 = {
-    '0': {
-        'nick': 'user0',
-        'total_points': 0,
-        'no_won': 0,
-        'no_played': 0
-    },
-    '1': {
-        'nick': 'user1',
-        'total_points': 0,
-        'no_won': 0,
-        'no_played': 0
-    },
-    '2': {
-        'nick': 'user2',
-        'total_points': 0,
-        'no_won': 0,
-        'no_played': 0
-    },
-    '3': {
-        'nick': 'user3',
-        'total_points': 0,
-        'no_won': 0,
-        'no_played': 0
-    },
-    '4': {
-        'nick': 'user4',
-        'total_points': 0,
-        'no_won': 0,
-        'no_played': 0
-    }
-}
-
-firebase_ref.getRef().child('top5').set(top5)
 
